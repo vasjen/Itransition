bdac894d279a42d34f18faad8bed51fffd38e831
Vasjen <vasjenm@gmail.com>
2023-06-08 14:56:19 +0400

    Update script paths

--- Task6/Messenger/Program.cs
+++ Task6/Messenger/Program.cs
@@ -42,5 +42,14 @@ app.UseEndpoints(endpoints =>
             {   
                 endpoints.MapHub<MessageHub>("/messageHub");
             });
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
 
+    var context = services.GetRequiredService<AppDbContext>();
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
+};
 app.Run();
